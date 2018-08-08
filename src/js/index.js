a283da09c587b412c8cd9f5c216175230485dfba
DESKTOP-I827G3G\cbw <861754186@qq.com>
2018-08-08 14:21:36 +0800

    使用其他人的框架,初步完成页面展示功能

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,34 @@
 $(
-    //设置box,用js方式展现
-    function () {
-        $('#box').dialog();
-    }
+
+
+
+    //这里有先后的问题,header先加载出来,body还没有初始化;所以这个时候需要用户自己去设置
+//这个是定义的界面
+//添加新的Tab页
+//     $("#navmenu").on("click", "a[data-url]", function(e) {
+//         e.preventDefault();
+//         var tabTitle = $(this).text();
+//         var tabUrl = $(this).data("url");
+//
+//         if($("#tt").tabs("exists", tabTitle)) { //判断该Tab页是否已经存在
+//             $("#tt").tabs("select", tabTitle);
+//         }else {
+//             $("#tt").tabs("add", {
+//                 title: tabTitle,
+//                 href: tabUrl,
+//                 closable: true
+//             });
+//         }
+//         $("#navmenu .active").removeClass("active");
+//         $(this).parent().addClass("active");
+//     });
+//
+// //解决闪屏的问题
+// window.setTimeout(function() {
+//     $("#layout").css("visibility", "visible");
+// }, 800)
+//
+
+
 
 );
