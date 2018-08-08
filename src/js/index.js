ac1ad427fce4a7d068eeef36fa6f4ef681a96665
DESKTOP-I827G3G\cbw <861754186@qq.com>
2018-08-08 15:24:47 +0800

    加上遮罩

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,16 @@
 $(
+    function ajaxLoading(){
+        $("<div class=\"datagrid-mask\"></div>").css({display:"block",width:"100%",height:$(window).height()}).appendTo("body");
+        $("<div class=\"datagrid-mask-msg\"></div>").html("正在处理，请稍候。。。").appendTo("body").css({display:"block",left:($(document.body).outerWidth(true) - 190) / 2,top:($(window).height() - 45) / 2});
+    },
+    function ajaxLoadEnd(){
+        $(".datagrid-mask").remove();
+        $(".datagrid-mask-msg").remove();
+    }
 
 
 
-    //这里有先后的问题,header先加载出来,body还没有初始化;所以这个时候需要用户自己去设置
+//这里有先后的问题,header先加载出来,body还没有初始化;所以这个时候需要用户自己去设置
 //这个是定义的界面
 //添加新的Tab页
 //     $("#navmenu").on("click", "a[data-url]", function(e) {
